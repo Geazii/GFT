258f59d247a4cdee303e5d52c544a4748607ad99
GZ <32210223+Geazii@users.noreply.github.com>
2022-04-05 18:01:43 -0300

    Add files via upload

---
+++ Teste/2 - Letras/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2___Letras
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroPL = 0;
+            int numeroSL = 0;
+            string Letra;
+
+            Console.WriteLine("Insira duas letras");
+            char PrimeiraLetra = Console.ReadLine();
+            char SegundaLetra = Console.ReadLine();
+
+
+            if (numeroPL < numeroSL)
+            {
+                Console.WriteLine("Não está em ordem alfabética");
+            }
+
+            //usar dicionario ou array
+
+            //converter letra em número
+
+            //comparar tamanho
+
+
+
+            switch (Letra)
+            {
+                case 1:
+                break;
+
+
+
+
+            }
+    }
+}
